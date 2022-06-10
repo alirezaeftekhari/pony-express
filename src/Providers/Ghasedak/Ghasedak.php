cf43a3e805cac1383a813046fc6e2170e06115f9
Alireza Eftekhari <alireza.eftekhari@digikala.com>
2022-06-10 22:53:35 +0430

    Providers mathed with Dispatcher methods

--- src/Providers/Ghasedak/Ghasedak.php
+++ src/Providers/Ghasedak/Ghasedak.php
@@ -3,22 +3,14 @@
 namespace PonyExpress\Providers\Ghasedak;
 
 use PonyExpress\Dispatcher\AbstractPonyExpressDispatcher;
-use PonyExpress\Utilities\RabbitMq\RabbitMqProducer;
-use PonyExpress\Helpers\JSON;
 
 class Ghasedak extends AbstractPonyExpressDispatcher
 {
     final const API_KEY = 'fb987e82b13dad3af68d64896f4c005a3a8f1abaa5a714e38096d62fcd862ffc';
     final const LINE_NUMBER = '10008566';
 
-    public function send()
+    public static function send(string $number, string $text)
     {
-        GhasedakApiSingleton::getInstance()->SendSimple($this->number, $this->text, self::LINE_NUMBER);
-    }
-
-    public function sendAsync()
-    {
-        RabbitMqProducer::sender('messages', JSON::encoder($this->number, $this->text));
-        $this->notify();
+        GhasedakApiSingleton::getInstance()->SendSimple($number, $text, self::LINE_NUMBER);
     }
 }
--- src/Providers/KaveNegar/KaveNegar.php
+++ src/Providers/KaveNegar/KaveNegar.php
@@ -3,22 +3,14 @@
 namespace PonyExpress\Providers\KaveNegar;
 
 use PonyExpress\Dispatcher\AbstractPonyExpressDispatcher;
-use PonyExpress\Utilities\RabbitMq\RabbitMqProducer;
-use PonyExpress\Helpers\JSON;
 
 class KaveNegar extends AbstractPonyExpressDispatcher
 {
     final const API_KEY = 'your api key';
     final const LINE_NUMBER = '10004346';
 
-    public function send()
+    public static function send(string $number, string $text)
     {
-        KaveNegarSingleton::getInstance()->Send($this->number, $this->text, self::LINE_NUMBER);
-    }
-
-    public function sendAsync()
-    {
-        RabbitMqProducer::sender('messages', JSON::encoder($this->number, $this->text));
-        $this->notify();
+        KaveNegarSingleton::getInstance()->Send(self::LINE_NUMBER, $number, $text);
     }
 }
