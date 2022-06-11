e9d8596564497836dc9c6fb5b85c573fe243f5ad
Alireza Eftekhari <alireza.eftekhari@digikala.com>
2022-06-11 11:26:26 +0430

    log colors and mysql store method finished

--- src/Utilities/Sms.php
+++ src/Utilities/Sms.php
@@ -4,6 +4,7 @@ namespace PonyExpress\Utilities;
 
 use Exception;
 use PonyExpress\Helpers\JSON;
+use PonyExpress\Utilities\Mysql\Mysql;
 
 class Sms
 {
@@ -17,14 +18,27 @@ class Sms
             $text = $decodedMessage['text'];
             $provider = $decodedMessage['provider'];
 
+            $mysql = new Mysql();
+
             try {
+                //send the sms
                 $provider::send($number, $text);
-                echo "Message successfully sent!".PHP_EOL;
-                echo "provider: $provider".PHP_EOL;
-                echo "number: $number | text: $text".PHP_EOL;
+
+                //save to db by sent status
+                $mysql->store($number, $text, $provider, 'sent');
+
+                //show log
+                echo "\e[32m Message successfully sent!".PHP_EOL;
+                echo "\e[32m provider: $provider".PHP_EOL;
+                echo "\e[32m number: $number | text: $text".PHP_EOL;
+
             } catch (Exception $exception) {
-                echo "****************** Attention: ******************".PHP_EOL;
-                echo $exception->getMessage();
+                //save to db by failed status
+                $mysql->store($number, $text, $provider, 'failed');
+
+                //show log
+                echo "\e[31m ****************** Attention: ******************".PHP_EOL;
+                echo "\e[31m ".$exception->getMessage().PHP_EOL;
             }
 
         };
