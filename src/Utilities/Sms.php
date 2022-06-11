1e1c5070a9457b7416a61108cc171acf6a64463e
Alireza Eftekhari <alireza.eftekhari@digikala.com>
2022-06-11 14:48:45 +0430

    logs changed

--- src/Utilities/Sms.php
+++ src/Utilities/Sms.php
@@ -32,6 +32,7 @@ class Sms
                 echo "\e[32m Message successfully sent!".PHP_EOL;
                 echo "\e[32m provider: $provider".PHP_EOL;
                 echo "\e[32m number: $number | text: $text".PHP_EOL;
+                echo PHP_EOL;
 
             } catch (Exception $exception) {
                 //send to the failed-messages queue
@@ -39,7 +40,11 @@ class Sms
 
                 //show log
                 echo "\e[31m ****************** Attention: ******************".PHP_EOL;
+                echo "\e[31m Message didn't sent successfully!".PHP_EOL;
+                echo "\e[31m provider: $provider".PHP_EOL;
+                echo "\e[31m number: $number | text: $text".PHP_EOL;
                 echo "\e[31m ".$exception->getMessage().PHP_EOL;
+                echo PHP_EOL;
             }
         };
     }
@@ -62,14 +67,17 @@ class Sms
                 echo "\e[32m The message that sent has stored in the database!".PHP_EOL;
                 echo "\e[32m provider: $provider".PHP_EOL;
                 echo "\e[32m number: $number | text: $text".PHP_EOL;
-
+                echo PHP_EOL;
             } catch (Exception $exception) {
                 $mysql->store($number, $text, $provider, 'failed');
 
                 //show log
                 echo "\e[31m ****************** Attention: ******************".PHP_EOL;
-                echo "\e[32m The message that sent has not stored in the database!".PHP_EOL;
+                echo "\e[31m The message that sent has not stored in the database!".PHP_EOL;
+                echo "\e[31m provider: $provider".PHP_EOL;
+                echo "\e[31m number: $number | text: $text".PHP_EOL;
                 echo "\e[31m ".$exception->getMessage().PHP_EOL;
+                echo PHP_EOL;
             }
         };
     }
@@ -92,14 +100,17 @@ class Sms
                 echo "\e[32m The message that failed to send has stored in the database!".PHP_EOL;
                 echo "\e[32m provider: $provider".PHP_EOL;
                 echo "\e[32m number: $number | text: $text".PHP_EOL;
-
+                echo PHP_EOL;
             } catch (Exception $exception) {
                 $mysql->store($number, $text, $provider, 'failed');
 
                 //show log
                 echo "\e[31m ****************** Attention: ******************".PHP_EOL;
-                echo "\e[32m The message that failed to send has not stored in the database!".PHP_EOL;
+                echo "\e[31m The message that failed to send has not stored in the database!".PHP_EOL;
+                echo "\e[31m provider: $provider".PHP_EOL;
+                echo "\e[31m number: $number | text: $text".PHP_EOL;
                 echo "\e[31m ".$exception->getMessage().PHP_EOL;
+                echo PHP_EOL;
             }
         };
     }
