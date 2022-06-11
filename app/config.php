cefbdee4064f568e1a3e31450c17b6e5b0549d05
Alireza Eftekhari <alireza.eftekhari@digikala.com>
2022-06-11 18:14:57 +0430

    micro framework added

---
+++ app/config.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'SERVE_DIRS' => ['assets'],
+    'DB_HOST' => '127.0.0.1',
+    'DB_USERNAME' => 'root',
+    'DB_PASSWORD' => 'qazQAZ12++',
+    'DB_NAME' => 'PonyExpress'
+];
---
+++ core/View.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Core;
+
+use Exception;
+
+class View
+{
+    public static function render(string $view, array $parameters = []): string
+    {
+        $viewPath = __DIR__.'/../app/Views/'.$view.'.html';
+        if (!file_exists($viewPath)) {
+            throw new Exception('View not found');
+        }
+        return preg_replace_callback(
+            '/{{ (.*?) }}/',
+            function ($parameter) use ($parameters) {
+                return $parameters[$parameter[1]] ?? '';
+            },
+            file_get_contents($viewPath)
+        );
+    }
+}
