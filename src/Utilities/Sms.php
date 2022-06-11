518a0d7622b60029bb326ff04a4f5d86d621db2e
Alireza Eftekhari <alireza.eftekhari@digikala.com>
2022-06-11 12:17:36 +0430

    send to the queue when the messages are successful or failed insted of direct store in mysql

--- src/Utilities/Sms.php
+++ src/Utilities/Sms.php
@@ -4,6 +4,7 @@ namespace PonyExpress\Utilities;
 
 use Exception;
 use PonyExpress\Helpers\JSON;
+use PonyExpress\Utilities\MessageBrokers\RabbitMq;
 use PonyExpress\Utilities\Mysql\Mysql;
 
 class Sms
@@ -24,8 +25,14 @@ class Sms
                 //send the sms
                 $provider::send($number, $text);
 
-                //save to db by sent status
-                $mysql->store($number, $text, $provider, 'sent');
+                //send to the sent-messages queue
+                RabbitMq::broker('sent-messages', JSON::encoder([
+                    "number" => $number,
+                    "text" => $text,
+                    "provider" => static::class,
+                    'status' => 'sent'
+                ]));
+//                $mysql->store($number, $text, $provider, 'sent');
 
                 //show log
                 echo "\e[32m Message successfully sent!".PHP_EOL;
@@ -33,8 +40,16 @@ class Sms
                 echo "\e[32m number: $number | text: $text".PHP_EOL;
 
             } catch (Exception $exception) {
+                //send to the failed-messages queue
+                RabbitMq::broker('failed-messages', JSON::encoder([
+                    "number" => $number,
+                    "text" => $text,
+                    "provider" => static::class,
+                    'status' => 'failed'
+                ]));
+
                 //save to db by failed status
-                $mysql->store($number, $text, $provider, 'failed');
+//                $mysql->store($number, $text, $provider, 'failed');
 
                 //show log
                 echo "\e[31m ****************** Attention: ******************".PHP_EOL;
