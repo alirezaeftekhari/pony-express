74cf5e1bd987bba871914fc0f5f6ebd521d45f63
Alireza Eftekhari <alireza.eftekhari@digikala.com>
2022-06-11 14:49:29 +0430

    Services moved to src/Services

--- SmsBroker.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__."/vendor/autoload.php";
-
-use PonyExpress\Providers\Ghasedak\Ghasedak;
-use PonyExpress\PonyExpress;
-
-$ponyExpress = new PonyExpress();
-$ponyExpress->sendAsync(new Ghasedak("09120635002", "IM FROM POLY EXPRESS"));
---
+++ src/Services/SuccessfulSmsStorage.php
@@ -0,0 +1,6 @@
+<?php
+require_once __DIR__."/../../vendor/autoload.php";
+
+use PonyExpress\Utilities\MessageBrokers\RabbitMq;
+
+RabbitMq::successfulSmsStorage('sent-messages');
