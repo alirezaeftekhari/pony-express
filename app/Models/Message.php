63c6baa9ca6507374483f4ca8d8d9177add7c863
Alireza Eftekhari <alireza.eftekhari@digikala.com>
2022-06-12 20:53:22 +0430

    change the classes names beacause of Mysql removed class and Controllers refactored

---
+++ app/Models/Message.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use PonyExpress\Utilities\DataBase\MessagesActions;
+
+class Message
+{
+    public static function read(mixed $number, mixed $text, mixed $provider, mixed $status): array
+    {
+        $sql = "select * from Messages where 1 ";
+
+        (isset($number) and $number     !== 'undefined' and !empty($number))    ? $sql .= " and number like :number" : $sql .= '';
+        (isset($text) and $text         !== 'undefined' and !empty($text))      ? $sql .= " and text like :text" : $sql .= '';
+        (isset($provider) and $provider !== 'undefined' and !empty($provider))  ? $sql .= " and provider = :provider" : $sql .= '';
+        (isset($status) and $status     !== 'undefined' and !empty($status))    ? $sql .= " and status = :status" : $sql .= '';
+
+        $messageActions = new MessagesActions();
+        return $messageActions->read($sql, $number, $text, $provider, $status);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace App\Models;
-use PonyExpress\Utilities\DataBase\Mysql;
+use PonyExpress\Utilities\DataBase\UserActions;
 
 class User
 {
     public static function getUser(string $username): array|bool
     {
-        $mysql = new Mysql();
+        $mysql = new UserActions();
         return $mysql->getUserByUserName($username);
     }
 }
