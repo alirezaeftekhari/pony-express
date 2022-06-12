f3f7c951e0edc2066547f69ff6a39c999ba87e7c
Alireza Eftekhari <alireza.eftekhari@digikala.com>
2022-06-12 15:11:12 +0430

    Services path changed

--- src/Services/SmsBroker.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__."/../../vendor/autoload.php";
-
-use PonyExpress\Providers\Ghasedak\Ghasedak;
-use PonyExpress\PonyExpress;
-
-$ponyExpress = new PonyExpress();
-$ponyExpress->sendAsync(new Ghasedak("09120635002", "This is a test"));
-$ponyExpress->sendAsync(new Ghasedak("09120635009", "This is a fail"));
