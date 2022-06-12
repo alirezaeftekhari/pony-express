ef7e7c80738b5343efdd7364680edeb9bbf7be7d
Alireza Eftekhari <alireza.eftekhari@digikala.com>
2022-06-12 18:52:15 +0430

    method name changed

--- app/routes.php
+++ app/routes.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Core\Router;
 
-Router::addRoute('/api/sms/send', [Controllers\SmsSenderController::class, 'send']);
-Router::addRoute('/api/sms/report', [Controllers\ReportController::class, 'report']);
-Router::addRoute('/', [Controllers\ReportController::class, 'index']);
+Router::addRoute('/api/sms/send', [Controllers\SmsSenderController::class, 'sendApi']);
+Router::addRoute('/api/sms/report', [Controllers\ReportController::class, 'reportApi']);
+
+Router::addRoute('/report', [Controllers\ReportController::class, 'report']);
