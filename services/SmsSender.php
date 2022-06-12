ad9cc17dcd0592586c5aa5c762c11d81b882fc41
Alireza Eftekhari <alireza.eftekhari@digikala.com>
2022-06-12 15:32:36 +0430

    filder name lowercase

--- services/SmsSender.php
+++ services/SmsSender.php
@@ -1,6 +1,9 @@
 <?php
 require_once __DIR__ . "/../vendor/autoload.php";
-
 use PonyExpress\Utilities\MessageBrokers\RabbitMq;
+
+//load the .env file
+$dotenv = Dotenv\Dotenv::createMutable(__DIR__.'/..');
+$dotenv->load();
 
 RabbitMq::smsSender('messages');
