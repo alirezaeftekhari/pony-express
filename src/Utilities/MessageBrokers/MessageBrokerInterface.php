193ac74c71f6d159d619a37919bc24a8a1c563dd
Alireza Eftekhari <alireza.eftekhari@digikala.com>
2022-06-13 11:58:53 +0430

    add failedSmsStorage method

--- src/Utilities/MessageBrokers/MessageBrokerInterface.php
+++ src/Utilities/MessageBrokers/MessageBrokerInterface.php
@@ -6,4 +6,5 @@ interface MessageBrokerInterface
     public static function broker(string $queueName, string $text);
     public static function smsSender(string $queueName);
     public static function successfulSmsStorage(string $queueName);
+    public static function failedSmsStorage(string $queueName);
 }
